d92b13edb512b2c57dace3d66f0cde607f099772
Argent77 <argent77@server.fake>
2015-05-16 20:10:49 +0200

    Consider Balthazar joining Charname in Afaaq's advices

--- DjinniCompanion/dialogs/A7Afaaq_generic.d
+++ DjinniCompanion/dialogs/A7Afaaq_generic.d
@@ -378,8 +378,8 @@ APPEND %afq_dialog%
     IF ~Dead("gromnir") !Dead("yaga01") RandomNum(15, 2)~ + TalkGeneric.Advice.Specific.Yaga.2    // about Yaga-Shura
     IF ~Dead("yaga01") !Dead("abazigal") RandomNum(16, 1)~ + TalkGeneric.Advice.Specific.Abazigal.1    // about Abazigal
     IF ~Dead("yaga01") !Dead("sendai") RandomNum(16, 2)~ + TalkGeneric.Advice.Specific.Sendai.1    // about Sendai
-    IF ~Dead("abazigal") Dead("sendai") !Dead("balth") RandomNum(16, 1)~ + TalkGeneric.Advice.Specific.Balthazar.1    // about Balthazar
-    IF ~Dead("abazigal") Dead("sendai") !Dead("balth") RandomNum(16, 2)~ + TalkGeneric.Advice.Specific.Balthazar.2    // about Balthazar
+    IF ~Dead("abazigal") Dead("sendai") !Dead("balth") Global("BalthazarFights", "GLOBAL", 0) RandomNum(16, 1)~ + TalkGeneric.Advice.Specific.Balthazar.1    // about Balthazar
+    IF ~Dead("abazigal") Dead("sendai") !Dead("balth") Global("BalthazarFights", "GLOBAL", 0) RandomNum(16, 2)~ + TalkGeneric.Advice.Specific.Balthazar.2    // about Balthazar
     IF ~Dead("yaga01") OR(2) !Dead("abazigal") !Dead("sendai") Global("A7TalkAboutSolarDream", "GLOBAL", 0) RandomNum(18, 1)~ + TalkGeneric.Advice.Specific.SolarDream.1    // about CHARNAME's dream-like situations with the Solar
     IF ~Dead("balth") RandomNum(18, 2)~ + TalkGeneric.Advice.Specific.SolarDream.2    // about CHARNAME's dream-like situations with the Solar (after Melissan's true self has been revealed)
   END
