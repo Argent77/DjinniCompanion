bf14c4f3d10bf5682a2f202a80cb15303f6d636e
Argent77 <argent77@server.fake>
2015-08-20 20:56:22 +0200

    Fixed misspelled trigger name in trademeet.d

--- DjinniCompanion/dialogs/trademeet.d
+++ DjinniCompanion/dialogs/trademeet.d
@@ -54,17 +54,17 @@ END
 // *** Taquee (The Dao in front of the tent) ***
 
 APPEND ~TRGENI02~
-  IF WEIGHT #-1 ~Name("A7Afaaq", LastTalkedToBy) Random(2, 2)~ Taquee.1
+  IF WEIGHT #-1 ~Name("A7Afaaq", LastTalkedToBy) RandomNum(2, 1)~ Taquee.1
     SAY @1457   /* Get out of my sight, djinni. */
     IF ~~ EXIT
   END
 
-  IF WEIGHT #-1 ~Name("A7Afaaq", LastTalkedToBy) Random(2, 1)~ Taquee.2
+  IF WEIGHT #-1 ~Name("A7Afaaq", LastTalkedToBy) RandomNum(2, 2)~ Taquee.2
     SAY @1458   /* You kind is not welcome here. Begone! */
     IF ~~ EXIT
   END
 
-  IF WEIGHT #-1 ~!Name("A7Afaaq", LastTalkedToBy) See("A7Afaaq") Random(3, 1)~ Taquee.3
+  IF WEIGHT #-1 ~!Name("A7Afaaq", LastTalkedToBy) See("A7Afaaq") RandomNum(3, 1)~ Taquee.3
     SAY @1459 /* Please keep your djinni pet on a short leash to avoid unnecessary distractions, mortal! */
     IF ~~ EXIT
   END
