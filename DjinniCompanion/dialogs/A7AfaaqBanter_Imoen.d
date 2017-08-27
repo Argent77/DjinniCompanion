9a77580f90795ec4e29debac04d061d032161013
Argent77 <argent77@server.fake>
2017-08-27 14:59:04 +0200

    Added banters for Imoen and Viconia

--- DjinniCompanion/dialogs/A7AfaaqBanter_Imoen.d
+++ DjinniCompanion/dialogs/A7AfaaqBanter_Imoen.d
@@ -1,29 +1,41 @@
 // *** Banters with Imoen ***
-// TODO
+// Traified
 
-// Dummy banter #1
-CHAIN IF ~Global("InToB", "GLOBAL", 0) Global("A7BanterImoen", "GLOBAL", 1)~ THEN IMOEN2J Imoen2.0
-  ~(TODO: Dummy banter #1...)~ DO ~SetGlobal("A7BanterImoen", "GLOBAL", 2)~
-  == A7AFAAQ ~...~
-  == IMOEN2J ~....~
+// Banter #1
+CHAIN IF ~Global("A7BanterImoen", "GLOBAL", 1)~ THEN IMOEN2J Imoen2.0
+  @14500 /* Hey Afaaq! Do you have any good stories to tell? */ DO ~IncrementGlobal("A7BanterImoen", "GLOBAL", 1)~
+  == A7AFAAQ @14501 /* I have seen many events since I arrived on this plane and even took part in some of them. Not all were pleasant, and some should be buried in oblivion forever. However, I can give you an account of my time in the service of Caliph Hasib if you desire. */
+  == IMOEN2J @14502 /* No, Afaaq. I want you to tell me a story. Something exciting or funny, or even romantic. The way you talk about your past sounds always so gloomy. */
+  == A7AFAAQ @14503 /* I think I understand your concern. Then let me tell you the tale of the Mystic Gardens of Almraiven. */
+  == IMOEN2J @14504 /* That sounds much better. But we shouldn't keep <CHARNAME> waiting too long. I'd love to listen to your tale when we set up camp tonight. */
+  == A7AFAAQ @14505 /* You are right, Imoen. The tale is quite deep and requires patience as well as a sharp mind to understand all the intricacies of its underlying philosophy. */
+  == IMOEN2J @14506 /* Afaaq?! */
+  == A7AFAAQ @14507 /* Oh, do not worry too much. You will enjoy the tale nonetheless. */
 EXIT
 
-CHAIN IF ~Global("InToB", "GLOBAL", 1) Global("A7BanterImoen", "GLOBAL", 1)~ THEN IMOEN25J Imoen2.0
-  ~(TODO: Dummy banter #1...)~ DO ~SetGlobal("A7BanterImoen", "GLOBAL", 2)~
-  == A7AFAAQ ~...~
-  == IMOEN25J ~....~
+CHAIN IF ~Global("A7BanterImoen", "GLOBAL", 1)~ THEN IMOEN25J Imoen2.0
+  @14500 /* Hey Afaaq! Do you have any good stories to tell? */ DO ~IncrementGlobal("A7BanterImoen", "GLOBAL", 1)~
+  == A7AFAAQ @14501 /* I have seen many events since I arrived on this plane and even took part in some of them. Not all were pleasant, and some should be buried in oblivion forever. However, I can give you an account of my time in the service of Caliph Hasib if you desire. */
+  == IMOEN25J @14502 /* No, Afaaq. I want you to tell me a story. Something exciting or funny, or even romantic. The way you talk about your past sounds always so gloomy. */
+  == A7AFAAQ @14503 /* I think I understand your concern. Then let me tell you the tale of the Mystic Gardens of Almraiven. */
+  == IMOEN25J @14504 /* That sounds much better. But we shouldn't keep <CHARNAME> waiting too long. I'd love to listen to your tale when we set up camp tonight. */
+  == A7AFAAQ @14505 /* You are right, Imoen. The tale is quite deep and requires patience as well as a sharp mind to understand all the intricacies of its underlying philosophy. */
+  == IMOEN25J @14506 /* Afaaq?! */
+  == A7AFAAQ @14507 /* Oh, do not worry too much. You will enjoy the tale nonetheless. */
 EXIT
 
 
-// Dummy banter #2
-CHAIN IF ~Name("Imoen2", LastTalkedToBy) Global("InToB", "GLOBAL", 0) Global("BanterActive", "LOCALS", 1) Global("A7BanterImoen", "GLOBAL", 2)~ THEN A7AFAAQ Imoen2.1
-  ~(TODO: Dummy banter #2...)~ DO ~SetGlobal("A7BanterImoen", "GLOBAL", 4)~
-  == IMOEN2J ~...~
-  == A7AFAAQ ~....~
-EXIT
-
-CHAIN IF ~Name("Imoen2", LastTalkedToBy) Global("InToB", "GLOBAL", 1) Global("BanterActive", "LOCALS", 1) Global("A7BanterImoen", "GLOBAL", 2)~ THEN A7AFAAQ Imoen2.1
-  ~(TODO: Dummy banter #2...)~ DO ~SetGlobal("A7BanterImoen", "GLOBAL", 4)~
-  == IMOEN25J ~...~
-  == A7AFAAQ ~....~
+// Banter #2
+CHAIN IF ~Global("A7BanterImoen", "GLOBAL", 3)~ THEN IMOEN25J Imoen2.1
+  @14550 /* Do you sometimes miss your home? */ DO ~IncrementGlobal("A7BanterImoen", "GLOBAL", 1)~
+  == A7AFAAQ @14551 /* I am not yet so long away from my home plane in djinni terms. We are solitary creatures by nature. It is not unusual for a djinn to journey alone for an age or two. */
+  = @14552 /* Do you have something on your mind? */
+  == IMOEN25J @14553 /* No, not really. I've been thinking about my home in Candlekeep recently. About what would have happened if I had not followed <CHARNAME> when <PRO_HESHE> left with Gorion. I would certainly not have been through all this torture from Irenicus, and more. */
+  == A7AFAAQ @14554 /* Having regrets about past decisions is a mortal quality. You have made your choices and were able to follow them through until now. Hardship is part of the journey. It molds your character and lets you become stronger and more experienced. */
+  == IMOEN25J @14555 /* I guess so. If I had stayed in Candlekeep then I would have had to flee from other Bhaalspawns eventually. I might have even ended in Saradush with all the others. A horrible thought. */
+  == A7AFAAQ @14556 /* That would have been one of countless paths you could have taken. Never forget that you can follow different branches at any given time. Having stayed at home would have as many possible outcomes as travelling with <CHARNAME>. */
+  == IMOEN25J @14557 /* Yeah, but many times I feel as if I have little choice in what to do or where to go. Being a Bhaalspawn is pretty inconvenient at times. */
+  == A7AFAAQ @14558 /* Your observation is not completely wrong. Many choices are made unconsciously, and sometimes they are influenced by other forces. Especially when your heritage comes into play. */
+  = @14559 /* We djinn strongly believe in Fate, the grand plan of the universe that guides and sometimes forces us to follow a certain direction. But that doesn't mean you have no control over the choices you make in life. On the contrary, it is up to you which path you will take and the universe will follow suit. */
+  == IMOEN25J @14560 /* That sounds really weird. But thank you anyway, Afaaq. I try to remember your words when I'm about to make another stupid choice. */
 EXIT
--- DjinniCompanion/dialogs/A7AfaaqBanter_Viconia.d
+++ DjinniCompanion/dialogs/A7AfaaqBanter_Viconia.d
@@ -1,69 +1,117 @@
 // *** Banters with Viconia ***
-// TODO
+// Traified
 
-// Dummy banter #1
-CHAIN IF ~Global("InToB", "GLOBAL", 0) Global("A7BanterViconia", "GLOBAL", 1)~ THEN VICONIJ Viconia.0
-  ~(TODO: Dummy banter #1...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 2)~
-  == A7AFAAQ ~...~
-  == VICONIJ ~....~
-EXIT
+APPEND ~A7AFAAQ~
+  // Banter #1
+  IF ~~ AfaaqViconia.0
+    SAY @20001 /* The path of life does not always follow a straight line, just as air can be felt as a light breeze at one time or a violent storm at another. I am merely weathering it out like I did countless times before. */
+    IF ~Global("InToB", "GLOBAL", 0)~ EXTERN VICONIJ ViconiaAfaaq.1
+    IF ~!Global("InToB", "GLOBAL", 0)~ EXTERN VICON25J ViconiaAfaaq.1
+  END
 
-CHAIN IF ~Global("InToB", "GLOBAL", 1) Global("A7BanterViconia", "GLOBAL", 1)~ THEN VICON25J Viconia.0
-  ~(TODO: Dummy banter #1...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 2)~
-  == A7AFAAQ ~...~
-  == VICON25J ~....~
-EXIT
+  IF ~~ AfaaqViconia.1
+    SAY @20005 /* False pride may cause more damage in the long term. You above all should know very well what I am talking about. */
+    IF ~Global("InToB", "GLOBAL", 0) GlobalLT("A7Quest3", "GLOBAL", 2)~ EXTERN VICONIJ ViconiaAfaaq.2a
+    IF ~Global("InToB", "GLOBAL", 0) GlobalGT("A7Quest3", "GLOBAL", 1)~ EXTERN VICONIJ ViconiaAfaaq.2b
+    IF ~!Global("InToB", "GLOBAL", 0) GlobalLT("A7Quest3", "GLOBAL", 2)~ EXTERN VICON25J ViconiaAfaaq.2a
+    IF ~!Global("InToB", "GLOBAL", 0) GlobalGT("A7Quest3", "GLOBAL", 1)~ EXTERN VICON25J ViconiaAfaaq.2b
+  END
 
-
-// Dummy banter #2
-CHAIN IF ~Name("Viconia", LastTalkedToBy) Global("InToB", "GLOBAL", 0) Global("BanterActive", "LOCALS", 1) Global("A7BanterViconia", "GLOBAL", 2)~ THEN A7AFAAQ Viconia.1
-  ~(TODO: Dummy banter #2...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 4)~
-  == VICONIJ ~...~
-  == A7AFAAQ ~....~
-EXIT
-
-CHAIN IF ~Name("Viconia", LastTalkedToBy) Global("InToB", "GLOBAL", 1) Global("BanterActive", "LOCALS", 1) Global("A7BanterViconia", "GLOBAL", 2)~ THEN A7AFAAQ Viconia.1
-  ~(TODO: Dummy banter #2...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 4)~
-  == VICON25J ~...~
-  == A7AFAAQ ~....~
-EXIT
+  IF ~~ AfaaqViconia.2
+    SAY @20008 /* Only time will tell if you made the right choice. */
+    IF ~~ EXIT
+  END
+END
 
 
+APPEND ~VICONIJ~
+  // Banter #1
+  IF ~Global("A7BanterViconia", "GLOBAL", 1)~ ViconiaAfaaq.0
+    SAY @20000 /* Afaaq, you are rather glum and submissive for a djinn. Has life broken you that much already? */
+    IF ~~ DO ~IncrementGlobal("A7BanterViconia", "GLOBAL", 1)~ EXTERN A7AFAAQ AfaaqViconia.0
+  END
 
-// Banter #1
-// CHAIN IF ~See("A7Afaaq") !StateCheck("A7Afaaq", CD_STATE_NOTVALID) Global("InToB", "GLOBAL", 0) GlobalGT("A7BanterIntro", "GLOBAL", 0) Global("A7BanterViconia", "GLOBAL", 0)~ THEN BVICONI Viconia.0
-  // ~(TODO: Viconia remembers that her house once put genies into service too, who were quite effective...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 1)~
-  // == A7AFAAQ IF ~GlobalLT("A7Quest3", "GLOBAL", 2)~ ~Afaaq returns that he already knows what kind of service is expected from the drow (vaguely if before Quest 3)~
-  // == A7AFAAQ IF ~GlobalGT("A7Quest3", "GLOBAL", 1)~ ~Afaaq returns that he already knows what kind of service is expected from the drow (more specific during or after Quest 3)~
-  // == BVICONI ~Viconia replies that he should rather feel lucky, as what doesn't kill one makes one stronger.~
-// EXIT
+  IF ~~ ViconiaAfaaq.1
+    SAY @20002 /* You are truly a disgrace to your kind, given that you have powers at your fingertips that would make every mortal green with envy. */
+    IF ~GlobalLT("A7Quest3", "GLOBAL", 2)~ + ViconiaAfaaq.1a
+    IF ~GlobalGT("A7Quest3", "GLOBAL", 1)~ + ViconiaAfaaq.1b
+  END
 
-// CHAIN IF ~See("A7Afaaq") !StateCheck("A7Afaaq", CD_STATE_NOTVALID) Global("InToB", "GLOBAL", 1) GlobalGT("A7BanterIntro", "GLOBAL", 0) Global("A7BanterViconia", "GLOBAL", 0)~ THEN BVICON25 Viconia.0
-  // ~(TODO: Viconia remembers that her house once put genies into service too, who were quite effective...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 1)~
-  // == A7AFAAQ IF ~GlobalLT("A7Quest3", "GLOBAL", 2)~ ~Afaaq returns that he already knows what kind of service is expected from the drow (vaguely if before Quest 3)~
-  // == A7AFAAQ IF ~GlobalGT("A7Quest3", "GLOBAL", 1)~ ~Afaaq returns that he already knows what kind of service is expected from the drow (more specific during or after Quest 3)~
-  // == BVICON25 ~Viconia replies that he should rather feel lucky, as what doesn't kill one makes one stronger.~
-// EXIT
+  IF ~~ ViconiaAfaaq.1a
+    SAY @20003 /* My House had once put genies into service. Despite our best efforts we could never break their will nor abate their pride. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.1
+  END
+
+  IF ~~ ViconiaAfaaq.1b
+    SAY @20004 /* My House had once put genies into service. Despite our best efforts we could never break their will nor abate their pride. Obviously someone else was more successful with you. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.1
+  END
+
+  IF ~~ ViconiaAfaaq.2a
+    SAY @20006 /* Pride allowed me to survive and made me stronger. If I had displayed such a meek behavior at that time I would have shared the fate of everyone else of my House. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.2
+  END
+
+  IF ~~ ViconiaAfaaq.2b
+    SAY @20007 /* Pride allowed me to survive and made me stronger. If I had displayed such a meek behavior at that time I would have shared the fate of everyone else of my House. You above all should know about Drow society. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.2
+  END
+END
+
+
+APPEND ~VICON25J~
+  // Banter #1
+  IF ~Global("A7BanterViconia", "GLOBAL", 1)~ ViconiaAfaaq.0
+    SAY @20000 /* Afaaq, you are rather glum and submissive for a djinn. Has life broken you that much already? */
+    IF ~~ DO ~IncrementGlobal("A7BanterViconia", "GLOBAL", 1)~ EXTERN A7AFAAQ AfaaqViconia.0
+  END
+
+  IF ~~ ViconiaAfaaq.1
+    SAY @20002 /* You are truly a disgrace to your kind, given that you have powers at your fingertips that would make every mortal green with envy. */
+    IF ~GlobalLT("A7Quest3", "GLOBAL", 2)~ + ViconiaAfaaq.1a
+    IF ~GlobalGT("A7Quest3", "GLOBAL", 1)~ + ViconiaAfaaq.1b
+  END
+
+  IF ~~ ViconiaAfaaq.1a
+    SAY @20003 /* My House had once put genies into service. Despite our best efforts we could never break their will nor abate their pride. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.1
+  END
+
+  IF ~~ ViconiaAfaaq.1b
+    SAY @20004 /* My House had once put genies into service. Despite our best efforts we could never break their will nor abate their pride. Obviously someone else was more successful with you. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.1
+  END
+
+  IF ~~ ViconiaAfaaq.2a
+    SAY @20006 /* Pride allowed me to survive and made me stronger. If I had displayed such a meek behavior at that time I would have shared the fate of everyone else of my House. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.2
+  END
+
+  IF ~~ ViconiaAfaaq.2b
+    SAY @20007 /* Pride allowed me to survive and made me stronger. If I had displayed such a meek behavior at that time I would have shared the fate of everyone else of my House. You above all should know about Drow society. */
+    IF ~~ EXTERN A7AFAAQ AfaaqViconia.2
+  END
+END
 
 
 // Banter #2
 // CHAIN IF ~Name("Viconia", LastTalkedToBy) Global("InToB", "GLOBAL", 0) Global("BanterActive", "LOCALS", 1) Global("A7BanterViconia", "GLOBAL", 1)~ THEN A7AFAAQ Viconia.1
   // ~(TODO: Afaaq reminds her that she too would either not survive or being held as slave if she decided to return to the drow....)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 2)~
-  // == BVICONI ~Viconia bluffs that she knows ways to avoid enslavement.~
+  // == VICONIJ ~Viconia bluffs that she knows ways to avoid enslavement.~
   // == A7AFAAQ ~Afaaq calls her bluff and tells her not to fool herself. She know that without Lolth's support she has not even a slight chance.~
-  // == BVICONI ~Viconia returns a vicious remark and cuts off the conversation.~
+  // == VICONIJ ~Viconia returns a vicious remark and cuts off the conversation.~
 // EXIT
 
 // CHAIN IF ~Name("Viconia", LastTalkedToBy) Global("InToB", "GLOBAL", 1) Global("BanterActive", "LOCALS", 1) Global("A7BanterViconia", "GLOBAL", 1)~ THEN A7AFAAQ Viconia.1
   // ~(TODO: Afaaq reminds her that she too would either not survive or being held as slave if she decided to return to the drow....)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 2)~
-  // == BVICON25 ~Viconia bluffs that she knows ways to avoid enslavement.~
+  // == VICON25J ~Viconia bluffs that she knows ways to avoid enslavement.~
   // == A7AFAAQ ~Afaaq calls her bluff and tells her not to fool herself. She know that without Lolth's support she has not even a slight chance.~
-  // == BVICON25 ~Viconia returns a vicious remark and cuts off the conversation.~
+  // == VICON25J ~Viconia returns a vicious remark and cuts off the conversation.~
 // EXIT
 
 
 // Banter #3
-// CHAIN IF ~See("A7Afaaq") !StateCheck("A7Afaaq", CD_STATE_NOTVALID) Global("InToB", "GLOBAL", 1) Global("A7AfaaqEnslaved", "GLOBAL", 0) Global("A7AfaaqUpgraded", "GLOBAL", 1) Global("A7BanterViconia", "GLOBAL", 2)~ THEN BVICON25 Viconia.2
+// CHAIN IF ~See("A7Afaaq") !StateCheck("A7Afaaq", CD_STATE_NOTVALID) Global("InToB", "GLOBAL", 1) Global("A7AfaaqEnslaved", "GLOBAL", 0) Global("A7AfaaqUpgraded", "GLOBAL", 1) Global("A7BanterViconia", "GLOBAL", 2)~ THEN VICON25J Viconia.2
   // ~(TODO: Viconia: tells Afaaq that she wouldn't have released him from his enslavement if she was in <CHARNAME>'s place and hints to the services she would have required of Afaaq...)~ DO ~SetGlobal("A7BanterViconia", "GLOBAL", 3)~
   // == A7AFAAQ ~Afaaq counters that no captivity would last eternally and genies aren't known to forgive mistreatments.~
 // EXIT
